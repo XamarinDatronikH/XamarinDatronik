fc38e15c229f5132cac5f16834732cf83c656f43
Hampus Lagerberg <hampus.lagerberg@consid.se>
2020-07-23 11:49:18 +0200

    initial

---
+++ XamarinDatronik/Views/CustomControls/DynamicCircularImage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace XamarinDatronik.Views
+{
+    public partial class DynamicCircularImage : ContentView
+    {
+        public static readonly BindableProperty IconSourceProperty = BindableProperty.Create(
+            nameof(IconSource), typeof(string), typeof(DynamicCircularImage));
+
+        public DynamicCircularImage()
+        {
+            InitializeComponent();
+        }
+
+        public string IconSource
+        {
+            get => (string)GetValue(IconSourceProperty);
+            set => SetValue(IconSourceProperty, value);
+        }
+
+        void ParentContainerSizeChanged(object sender, EventArgs e)
+        {
+            var width = Width - Padding.HorizontalThickness;
+            var height = Height - Padding.VerticalThickness;
+
+            if (width == 0 || height == 0) return;
+
+            if (width > height)
+            {
+                MainFrame.WidthRequest = height;
+                MainFrame.HorizontalOptions = LayoutOptions.Center;
+                MainFrame.CornerRadius = (float)height / 2;
+            }
+            else if (height > width)
+            {
+                MainFrame.HeightRequest = width;
+                MainFrame.VerticalOptions = LayoutOptions.Center;
+                MainFrame.CornerRadius = (float)width / 2;
+            }
+            else
+                MainFrame.CornerRadius = (float)width / 2;
+        }
+    }
+}
---
+++ XamarinDatronik/Views/SlideInAnimationDemo.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XamarinDatronik.Views
+{
+    public partial class SlideInAnimationDemo
+    {
+        public SlideInAnimationDemo()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            ImageOne.Opacity = 0;
+            ImageTwo.Opacity = 0;
+            ImageThree.Opacity = 0;
+
+            var animation = new Animation
+            {
+                {
+                    0,
+                    .33,
+                    new Animation(v => ImageOne.TranslationX = v, -300, 0)
+                },
+                {
+                    0,
+                    .33,
+                    new Animation(v => ImageOne.Opacity = v, 0, 1)
+                },
+                {
+                    .33,
+                    .66,
+                    new Animation(v => ImageTwo.TranslationX = v, 300, 0)
+                },
+                {
+                    .33,
+                    .66,
+                    new Animation(v => ImageTwo.Opacity = v, 0, 1)
+                },
+                {
+                    .66,
+                    1,
+                    new Animation(v => ImageThree.TranslationX = v, -300,0)
+                },
+                {
+                    .66,
+                    1,
+                    new Animation(v => ImageThree.Opacity = v, 0,1)
+                }
+            };
+
+            animation.Commit(this, "SlideIn", length: 1500, easing: Easing.Linear);
+        }
+    }
+}
